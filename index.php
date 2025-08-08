a418e58444a52c7bffc418a4ef4d72beae8ee43b
OlivierBrun82 <contactpro@olivierbrun.eu>
2025-08-08 13:49:13 +0200

    Initialisation de l'exercice, création d'une structure et import de la logique de connection à la db

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'config/database.php';
+
+    echo"afficher les task";
+
+
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1><strong>Liste des tâches</strong></h1>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <span>Au boulot!</span>
+    </footer>
+</body>
+</html>
